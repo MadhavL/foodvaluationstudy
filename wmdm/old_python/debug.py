4f15b63fa7402195be81f57088d47eda431c0e80
MadhavL <madhav.lavakare@yale.edu>
2024-03-13 18:47:10 -0400

    Deleted files by mistake

--- wmdm/old_python/debug.py
+++
@@ -1,8 +0,0 @@
-from psiturk.experiment import app
-
-host = "0.0.0.0"
-port = 5000
-print("Serving on ", "http://" + host + ":" + str(port))
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.jinja_env.auto_reload = True
-app.run(debug=True, host=host, port=port)
